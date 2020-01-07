c2d054f67fbb3ba87c4717eb015718f2027db66e
Daniella Salles <daniellac.salles@gmail.com>
2020-01-07 17:59:24 -0300

    Atualização BD

--- form_cadastro.sql
+++ form_cadastro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 04-Jan-2020 às 22:59
+-- Tempo de geração: 07-Jan-2020 às 21:56
 -- Versão do servidor: 10.4.8-MariaDB
 -- versão do PHP: 7.1.33
 
@@ -37,6 +37,16 @@ CREATE TABLE `endereco` (
   `ID_logradouro` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `endereco`
+--
+
+INSERT INTO `endereco` (`ID_endereco`, `cep`, `logradouro`, `numero`, `complemento`, `ID_logradouro`) VALUES
+(34, 30775570, 'Barão de Ibituruna', 602, '', 1),
+(35, 31630190, 'Eduardo Antônio da Fonseca', 973, '', 1),
+(36, 31720210, 'Mário Batista da Costa', 146, '', 1),
+(37, 30451451, 'Saul', 154, '', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -48,6 +58,18 @@ CREATE TABLE `escolaridade` (
   `escolaridade` enum('Fundamental incompleto','Fundamental completo','Médio incompleto','Médio completo','Superior incompleto','Superior completo') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `escolaridade`
+--
+
+INSERT INTO `escolaridade` (`ID_escolaridade`, `escolaridade`) VALUES
+(1, 'Fundamental incompleto'),
+(2, 'Fundamental completo'),
+(3, 'Médio incompleto'),
+(4, 'Médio completo'),
+(5, 'Superior incompleto'),
+(6, 'Superior completo');
+
 -- --------------------------------------------------------
 
 --
@@ -58,6 +80,17 @@ CREATE TABLE `estado_civil` (
   `ID_estcivil` int(11) NOT NULL,
   `estadocivil` enum('Solteiro','Casado','Separado','Divorciado','Viúvo') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `estado_civil`
+--
+
+INSERT INTO `estado_civil` (`ID_estcivil`, `estadocivil`) VALUES
+(1, 'Solteiro'),
+(2, 'Casado'),
+(3, 'Separado'),
+(4, 'Divorciado'),
+(5, 'Viúvo');
 
 -- --------------------------------------------------------
 
@@ -78,6 +111,16 @@ CREATE TABLE `pessoa` (
   `informatica` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `pessoa`
+--
+
+INSERT INTO `pessoa` (`ID_pessoa`, `nome`, `datanasc`, `cpf`, `ID_estcivil`, `ID_escolaridade`, `ID_endereco`, `informacoes`, `ingles`, `informatica`) VALUES
+(253, 'Natália Alícia Bruna Caldeira', '1960-10-03', '80445745657', 3, 4, 34, '', 'Sim', NULL),
+(254, 'Joana Fernanda da Mata', '1972-05-17', '43508413691', 1, 6, 35, '', 'Sim', 'Sim'),
+(255, 'Juliana Camila Assis', '1988-07-19', '85491421669', 1, 5, 36, '', NULL, 'Sim'),
+(256, 'Mirella Vanessa Peixoto', '1990-03-09', '36184671685', 1, 4, 37, '', NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -88,6 +131,15 @@ CREATE TABLE `tipo_logradouro` (
   `ID_logradouro` int(11) NOT NULL,
   `tipologradouro` enum('Rua','Avenida','Praça','') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tipo_logradouro`
+--
+
+INSERT INTO `tipo_logradouro` (`ID_logradouro`, `tipologradouro`) VALUES
+(1, 'Rua'),
+(2, 'Avenida'),
+(3, 'Praça');
 
 --
 -- Índices para tabelas despejadas
@@ -135,31 +187,31 @@ ALTER TABLE `tipo_logradouro`
 -- AUTO_INCREMENT de tabela `endereco`
 --
 ALTER TABLE `endereco`
-  MODIFY `ID_endereco` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_endereco` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT de tabela `escolaridade`
 --
 ALTER TABLE `escolaridade`
-  MODIFY `ID_escolaridade` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_escolaridade` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT de tabela `estado_civil`
 --
 ALTER TABLE `estado_civil`
-  MODIFY `ID_estcivil` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_estcivil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de tabela `pessoa`
 --
 ALTER TABLE `pessoa`
-  MODIFY `ID_pessoa` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_pessoa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=257;
 
 --
 -- AUTO_INCREMENT de tabela `tipo_logradouro`
 --
 ALTER TABLE `tipo_logradouro`
-  MODIFY `ID_logradouro` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_logradouro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restrições para despejos de tabelas
